5bf59fc68a3b4b6bfe159b184d019fadd13d9f2d
Lu Zhuo <125237595+TAKAGIVEGETA@users.noreply.github.com>
2026-01-20 21:25:46 +0800

    init

---
+++ lingoDuo/duolingo-time.js
@@ -0,0 +1,13 @@
+try {
+    let obj = JSON.parse($response.body);
+    let s = parseInt($argument.minutes) * 60;
+    if (!isNaN(s) && s > 0 && obj.liveOpscs)
+        obj.liveOpscs.forEach(c => {
+            if (c.initialTime) c.initialTime = s;
+            if (c.initialLevelTimes) c.initialLevelTimes = c.initialLevelTimes.map(() => s);
+            if (c.initialSessionTimes) c.initialSessionTimes = c.initialSessionTimes.map(() => s);
+        });
+    $done({ body: JSON.stringify(obj) });
+} catch (e) {
+    $done({});
+}
