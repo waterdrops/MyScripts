a4289774d9e9ed667c7e4e1011183109ac09e4e2
Lu Zhuo <125237595+TAKAGIVEGETA@users.noreply.github.com>
2026-01-22 20:31:32 +0800

    upt

--- lingoDuo/lingoDuo-xaM-enhanced.js
+++ lingoDuo/lingoDuo-xaM-enhanced.js
@@ -51,23 +51,22 @@ try {
                     xpBoostMultiplier: xpMultiplier
                 });
         }
-
-        if (userdata.currentCourse && Array.isArray(userdata.currentCourse.pathSectioned)) {
-            userdata.currentCourse.pathSectioned.forEach(section => {
-                if (Array.isArray(section.units)) {
-                    section.units.forEach(unit => {
-                        if (Array.isArray(unit.levels)) {
-                            unit.levels.forEach(level => {
-                                if (level.state === 'locked') {
-                                    level.state = 'active';
-                                }
-                            });
-                        }
-                    });
-                }
-            });
+        const unlockAllEnabled = $argument.unlock_all;
+        if (unlockAllEnabled) {
+            if (userdata.currentCourse && Array.isArray(userdata.currentCourse.pathSectioned)) {
+                userdata.currentCourse.pathSectioned.forEach(section => {
+                    if (Array.isArray(section.units)) {
+                        section.units.forEach(unit => {
+                            if (Array.isArray(unit.levels)) {
+                                unit.levels.forEach(level => {
+                                    if (level.state === 'locked') level.state = 'active';
+                                });
+                            }
+                        });
+                    }
+                });
+            }
         }
-        
         obj.responses[0].body = JSON.stringify(userdata);
     }
     $done({ body: JSON.stringify(obj) });
