b660a39980b2f0fbd417a041e80cb9ff920eeb6b
Lu Zhuo <125237595+TAKAGIVEGETA@users.noreply.github.com>
2026-01-22 19:01:09 +0800

    upt

--- lingoDuo/lingoDuo-clear.js
+++ lingoDuo/lingoDuo-clear.js
@@ -6,9 +6,26 @@ try {
         'adaptiveInterleavedChallenges',
         'mistakesReplacementChallenges'
     ];
+    let isModified = false;
     pools.forEach(pool => {
-        if (obj[pool] && obj[pool].length > 0) obj[pool] = [];
+        if (obj[pool] && Array.isArray(obj[pool])) {
+            obj[pool] = [];
+            isModified = true;
+        }
     });
+    if (isModified && obj.trackingProperties) {
+        let tp = obj.trackingProperties;
+        Object.keys(tp).forEach(key => {
+            if (key.startsWith('num_challenges_') || 
+                key.startsWith('num_adaptive_') || 
+                key === 'expected_length' ||
+                key === 'sentences_count' ||
+                key === 'distinct_sentences_count') tp[key] = 0;
+        });
+        tp.expected_length = 0;
+        tp.num_challenges_gt = 0;
+        tp.num_challenges_generated = 0;
+    }
     $done({ body: JSON.stringify(obj) });
 } catch (e) {
     $done({});
