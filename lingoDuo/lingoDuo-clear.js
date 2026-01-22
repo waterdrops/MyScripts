9972d6a68c2978b99277d74d4f8e024968fed54c
Lu Zhuo <125237595+TAKAGIVEGETA@users.noreply.github.com>
2026-01-22 18:35:09 +0800

    upt

--- lingoDuo/lingoDuo-clear.js
+++ lingoDuo/lingoDuo-clear.js
@@ -1,12 +1,15 @@
 try {
-  let obj = JSON.parse($response.body);
-  const type = obj.type;
-  const skipTypes = ["SIDE_QUEST_RAMP_UP_PRACTICE", "SIDE_QUEST_MATCH_PRACTICE", "MATCH_PRACTICE"];
-  if (type && !skipTypes.includes(type)) {
-    if (obj.challenges) obj.challenges = [];
-    if (obj.expected_length) obj.expected_length = 0;
-  }
-  $done({ body: JSON.stringify(obj) });
+    let obj = JSON.parse($response.body);
+    const pools = [
+        'challenges',
+        'adaptiveChallenges',
+        'adaptiveInterleavedChallenges',
+        'mistakesReplacementChallenges'
+    ];
+    pools.forEach(pool => {
+        if (obj[pool] && obj[pool].length > 0) obj[pool] = [];
+    });
+    $done({ body: JSON.stringify(obj) });
 } catch (e) {
-  $done({});
+    $done({});
 }
