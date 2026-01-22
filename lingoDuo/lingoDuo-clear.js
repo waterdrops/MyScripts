ca02a0fc3556cc1da5f97e745b95b8318c3065d1
Lu Zhuo <125237595+TAKAGIVEGETA@users.noreply.github.com>
2026-01-22 19:05:59 +0800

    upt

--- lingoDuo/lingoDuo-clear.js
+++ lingoDuo/lingoDuo-clear.js
@@ -1,32 +1,19 @@
 try {
-    let obj = JSON.parse($response.body);
+  let obj = JSON.parse($response.body);
+  const type = obj.type;
+  const skipTypes = ["SIDE_QUEST_RAMP_UP_PRACTICE", "MATCH_PRACTICE"];
+  if (type && !skipTypes.includes(type)) {
     const pools = [
         'challenges',
         'adaptiveChallenges',
         'adaptiveInterleavedChallenges',
         'mistakesReplacementChallenges'
     ];
-    let isModified = false;
     pools.forEach(pool => {
-        if (obj[pool] && Array.isArray(obj[pool])) {
-            obj[pool] = [];
-            isModified = true;
-        }
+        if (obj[pool] && obj[pool].length > 0) obj[pool] = [];
     });
-    if (isModified && obj.trackingProperties) {
-        let tp = obj.trackingProperties;
-        Object.keys(tp).forEach(key => {
-            if (key.startsWith('num_challenges_') || 
-                key.startsWith('num_adaptive_') || 
-                key === 'expected_length' ||
-                key === 'sentences_count' ||
-                key === 'distinct_sentences_count') tp[key] = 0;
-        });
-        tp.expected_length = 0;
-        tp.num_challenges_gt = 0;
-        tp.num_challenges_generated = 0;
-    }
-    $done({ body: JSON.stringify(obj) });
+  }
+  $done({ body: JSON.stringify(obj) });
 } catch (e) {
-    $done({});
+  $done({});
 }
